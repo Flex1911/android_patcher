027dfd8b378a0808bfe00a72914927041026d05f
Artem Borisov <dedsa2002@gmail.com>
2018-06-11 02:25:03 +0300

    Update for AOSP

--- patch.sh
+++ patch.sh
@@ -2,8 +2,17 @@
 
 ROOT="${PWD}"
 REPOSITORIES=(
+    'bionic'
+    'bootable/recovery'
+    'build/make'
+    'build/soong'
+    'external/selinux'
+    'frameworks/base'
     'frameworks/native'
+    'frameworks/opt/telephony'
     'packages/apps/Camera2'
+    'system/core'
+    'system/sepolicy'
 )
 
 for repository in "${REPOSITORIES[@]}"; do
